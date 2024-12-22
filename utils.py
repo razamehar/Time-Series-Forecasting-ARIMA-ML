4e01b36efc90cb9ab6c4e1058aefc433846e5adb
Raza Mehar <raza.mehar@gmail.com>
2024-12-22 17:41:47 +0100

    initial commit

---
+++ utils.py
@@ -0,0 +1,26 @@
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+
+
+def data_review(df):
+    info = {
+      'Number of Rows': df.shape[0],
+      'Number of Columns': df.shape[1],
+      'Missing Values': df.isnull().sum().sum(),
+      'Duplicate Values': df.duplicated().sum(),
+    }
+    return info
+
+
+def plot_series(series, array_like, label='', title=''):
+    data_range = range(len(series))
+    plt.plot(data_range, array_like, label=label)
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
+
+
+def calculate_mae(y_true, y_pred, title):
+    mae = mean_absolute_error(y_true, y_pred)
+    print(f'{title}:')
+    print('MAE:', mae)
